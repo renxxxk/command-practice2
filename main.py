553c426fcaee1ad5fd464279fc832e066d196215
kokubunren <r.kokubun.sys@morijyobi.ac.jp>
2021-03-26 11:34:51 +0900

    コミットの修正

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ height0 = float(input('あなたの身長を入力してください(cm): '))
 height = height0/100
 bj = calc_bmi.bmi(weight, height)
 print("----" * 10)
+print("あなたのBMI：" + str(bj[0]) + '\nあなたの体型: ' + bj[1])
 print("あなたのBMI: " + str(bj[0]) + '\nあなたの体型: ' + bj[1])
